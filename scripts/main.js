89ceae584b736990d94f267b7afaa46c0d4ab2c6
A16016344 <a16016344@alumnos.uady.mx>
2020-07-28 23:49:56 -0500

    Primer commit de la pagina

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+      }
+});
+
+const categorias = document.querySelectorAll("#categorias a");
+categorias.forEach((categoria) => {
+    categoria.addEventListener('click', (opcion) => {
+        opcion.preventDefault();
+        categorias.forEach((categoria) => categoria.classList.remove("activo"));
+        opcion.target.classList.add("activo");
+
+        const categoria = opcion.target.innerHTML.toLowerCase();
+        console.log(categoria);
+        categoria === "todos" ? grid.filter(`[data-categoria]`) : grid.filter(`[data-categoria="${categoria}"]`);
+    });
+});
+
+document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+    const busqueda = evento.target.value.toLowerCase()
+    grid.filter((item) => item.getElement().innerHTML.toLowerCase().includes(busqueda));
+})
+
+const items = document.querySelectorAll(".item-contenido");
+const overlay = document.querySelector('.overlay');
+items.forEach((item)=>{
+    item.addEventListener('click', (evento)=>{
+        console.log(item.innerHTML);
+        document.querySelector('.card').innerHTML = item.innerHTML
+        document.querySelector('.overlay').classList.remove('ocultar')
+    });
+});
+
+overlay.addEventListener('click', (e)=>{
+    
+    e.target.id === 'overlay' || 'cerrar' ? overlay.classList.add('ocultar') : '';
+});
