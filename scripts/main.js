7655550d50036a93f3cc59f13172cfe68a4f69cd
A16016344 <a16016344@alumnos.uady.mx>
2020-07-31 12:22:01 -0500

    Se convierte a pwa

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ const grid = new Muuri('.grid', {
       }
 });
 
+var nombre = recuperarInfo("nombre")
+var direccion = recuperarInfo("direccion_completa")
+
 const categorias = document.querySelectorAll("#categorias a");
 categorias.forEach((categoria) => {
     categoria.addEventListener('click', (opcion) => {
@@ -24,13 +27,37 @@ document.querySelector('#barra-busqueda').addEventListener('input', (evento) => 
 
 const items = document.querySelectorAll(".item-contenido");
 const overlay = document.querySelector('.overlay');
+const card = document.querySelector('.card')
+var paquete = "";
 items.forEach((item)=>{
     item.addEventListener('click', (evento)=>{
-        document.querySelector('.card').innerHTML = item.innerHTML
-        document.querySelector('.overlay').classList.remove('ocultar')
+        card.innerHTML = item.innerHTML;
+        card.innerHTML.includes("paquete") ? paquete = `Paquete%20${card.querySelector(".paquete").innerText.split(" ").join("%20")}` : "";
+        card.innerHTML.includes("Ingredientes a escoger") ? paquete += `%20_Ingredientes:_` : "";
+        card.innerHTML.includes("ocultar") ? paquete = card.querySelector(".ocultar").innerText.split(" ").join("%20") : "";
+        document.querySelector('.overlay').classList.remove('ocultar');
+        document.querySelector('#pedir').href = `https://wa.me/529993639814?&text=*Nombre%3A*%20${nombre}%0A*Dirección%3A*%20${direccion}%0A*Pedido%3A*%20${paquete}`
     });
 });
 
 overlay.addEventListener('click', (e)=>{
     e.target.id === 'overlay' || e.target.id === 'cerrar' ? overlay.classList.add('ocultar') : '';
 });
+
+function recuperarInfo(dato) {
+    if (localStorage.getItem(dato)){
+        return localStorage.getItem(dato).split(" ").join("%20");
+    } else {
+        return "";
+    }
+
+}
+
+(function() {
+    // TODO add service worker code here
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./service-worker.js')
+               .then(function() { console.log('Service Worker Registered'); });
+    }
+  })();
